353644402f87cd551ffe0fcdd4abb4c575b0f427
lutfensus <tufanyesil@protonmail.com>
2025-04-11 13:18:55 +0200

    init

--- src/LibraryMedia.java
+++ src/LibraryMedia.java
@@ -1,9 +1,9 @@
 public class LibraryMedia {
-    String title;
-    int publicationYear;
-    Boolean available;
+    protected String title;
+    protected int publicationYear;
+    protected Boolean available;
     public LibraryMedia(String title, int publicationYear) {}
-    public void borrot(){
+    public void borrow(){
         if(available == true){
 
         }
